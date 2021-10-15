aa5a62b5f3c7fddfebee48ab83d6db7603811b26
Usman Haider <leo14haider@gmail.com>
2021-10-15 09:28:50 +0500

    add article model field validations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_012319) do
+ActiveRecord::Schema.define(version: 2021_10_15_031457) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     title { "Sample Title" }
     content { "Sample Content" }
+    slug { "Sample Slug" }
   end
 end
